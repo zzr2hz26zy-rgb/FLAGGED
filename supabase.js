68db2c159ab1b6eb32fc714e334384f90156b3af
zzr2hz26zy-rgb <zzr2hz26zy@privaterelay.appleid.com>
2026-09-06 23:45:48 +0200

    Fix Supabase client name

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://ltstvsuaiwxbrfwemxgj.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_8EGcp4kDuunVq_a2Yq8EQA_DpOJjV_B";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_PUBLISHABLE_KEY
 );
